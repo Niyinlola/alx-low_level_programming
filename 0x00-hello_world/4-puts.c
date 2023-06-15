83a212f8b5302f129a9b6daf809b65400b8672e5
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-15 15:07:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point using the puts function
+ *
+ * Return: 0 (Success)
+ */
+int main:(void)
+{
+puts("\"Programming is like building a multilingual puzzle"\n)
+return (0);
+}
