57859ecfa1cbe22d4a91235d23ef94bd26c39170
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-16 06:57:00 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point for a 32bit and 64bit machine
+ * main - Entry point for a 32bit machine
  *
  * Return: 0 (Success)
  */
@@ -13,3 +13,19 @@ printf("Size of long long int: %lu byte(s)\n");
 printf("Size of float: %lu byte(s)\n");
 return (0);
 }
+#include <stdio.h>
+/**
+ * main - Entry point for a 64bit machine
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char: %lu byte(s)\n");
+printf("Size of int: %lu byte(s)\n");
+printf("Size of long int: %lu byte(s)\n");
+printf("Size of long long int: %lu byte(s)\n");
+printf("Size of float: %lu byte(s)\n");
+return (0);
+}
+~
