72a21a1999e867e5d603f25712ccadf88bcef13e
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-16 07:01:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,18 +13,4 @@ printf("Size of long long int: %lu byte(s)\n");
 printf("Size of float: %lu byte(s)\n");
 return (0);
 }
-/**
- * main - Entry point for a 64bit machine
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-printf("Size of char: %lu byte(s)\n");
-printf("Size of int: %lu byte(s)\n");
-printf("Size of long int: %lu byte(s)\n");
-printf("Size of long long int: %lu byte(s)\n");
-printf("Size of float: %lu byte(s)\n");
-return (0);
-}
 ~
