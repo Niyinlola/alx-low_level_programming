70da0a6950abb7ffe624a4460c6877012ff217f4
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-16 06:41:35 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
+ * main - Entry point for a 32bit machine
  *
- * Return : 0 
+ * Return: 0 (Success)
  */
 int main(void)
 {
-printf("Size of char: %i byte(s)\n", sizeof(char));
-printf("Size of int: %i byte(s)\n", sizeof(int));
-printf("Size of long int: %i byte(s)\n", sizeof(long int));
-printf("Size of long long int: %i byte(s)\n", sizeof(long long int));
-printf("Size of float: %i byte(s)\n", sizeof(float));
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
+
+/**
+ * main - Entry point for a 64bit machine
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
 
