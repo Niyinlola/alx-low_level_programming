1b6f43ae390f92a41be1889462ea96d058f72ee9
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-16 18:51:11 -0700

    Task 0 to Task 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-#include < stdlib.h>
+#include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - Entry
+ * main - Entry point
  * Return: 0 (Success)
  */
 int main(void)
 {
-int n;
-int z;
+	int n;
+	int z;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-z = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	z = n % 10;
 	if (z > 5)
 {
 	printf("The last digit of %d is %d and is greater than 5\n", n, z);
@@ -22,7 +22,7 @@ z = n % 10;
 {
 	printf("The last digit of %d is %d and is 0\n", n, z);
 }
-	else
+	if (z < 6 && z != 0)
 {
 	printf("The last digit of %d is %d and is less than 6 and not 0\n", n, z);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'y')
+		{
+			putchar(b);
+			b++;
+		}
+	putchar ('\n');
+	return (0);
+}
