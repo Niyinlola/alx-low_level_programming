ec10cb907b9f0c153d972e1fe53c812548bbcc5d
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-17 20:27:34 -0700

    Print combination of single digit numbers seperated by comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,18 @@ int main(void)
 {
 	int y;
 
-	for (y = 0; y < 10; y++)
+	for (y = 0; y <= 9; y++)
 	{
-		putchar(y);
-	}
+		putchar(y + '0');
 
 	if (y != 9)
+
 	{
-		putchar('.');
+		putchar(',');
 		putchar(' ');
 	}
-
+	}
 	putchar('\n');
+
 	return (0);
 }
