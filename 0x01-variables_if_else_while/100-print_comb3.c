d247ced2288e2f479a1013a91edd86a6cada6fb1
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-19 08:40:52 -0700

    Print combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		while (num2 <= '9')
 		{
-			if (!(num1 < num2) || num1 == num2)
+			if (num1 < num2 &&  num1 != num2)
 
 			{
 				putchar(num1);
@@ -31,7 +31,7 @@ int main(void)
 			}
 			num2++;
 		}
-		num2 = num1 + '2';
+		num2 = '0';
 		num1++;
 	}
 return (0);
