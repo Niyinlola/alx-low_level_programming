6530cf30c7d1a2c7cfb7de3be09d37f455870a39
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-20 05:37:04 -0700

    Print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,17 @@
 void print_alphabet_x10(void)
 {
 	char alphabet;
-	int i;
-	i = '0';
+	int i = 0;
+
 	while (i < 10)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
+		alphabet++;
+
 		_putchar('\n');
-	i++;
+		i++;
 	}
 }
