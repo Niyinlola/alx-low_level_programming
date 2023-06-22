25b916abbffb4c847b9d294c6c76fd900e091ce5
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-22 03:39:34 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	long unsigned int num3 = 0;
-	long unsigned int num5 = 0;
-	long unsigned int sum = 0;
-	long unsigned int y;
+	unsigned long int num3 = 0;
+	unsigned long int num5 = 0;
+	unsigned long int sum = 0;
+	unsigned long int y;
 
-	for (y = 0; y < 1024; y++)
+	for (y = 0; y < 1024; ++y)
 	{
 	if ((y % 3) == 0)
 	{
