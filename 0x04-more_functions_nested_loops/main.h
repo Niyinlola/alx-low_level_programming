adf617527b9e3c66c6405cde4b6542f91d20b86f
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-23 15:06:24 -0700

    Task 1.isdigit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 int _isupper(int c);
-int _putchar(char);
 int _isdigit(int c);
+int _putchar(char);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
