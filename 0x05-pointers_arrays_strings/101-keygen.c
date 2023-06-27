c5367865390f340819d9cd4894da68a25d464650
Niyinlola <niyinlolaesther10@gmail.com>
2023-06-26 17:41:29 -0700

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,2 +1,35 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that generates random valid passwords
+ * for the program 101-crackme
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int y, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (y = 0; y < 100; y++)
+	{
+		pass[y] = rand() % 78;
+		sum += (pass[y] + '0');
+		putchar(pass[y] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+		n = 2772 - sum - '0';
+		sum += n;
+		putchar(n + '0');
+		break;
+	}
+}
+
+	return (0);
+}
 
